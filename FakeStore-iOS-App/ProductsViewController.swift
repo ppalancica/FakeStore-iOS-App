0fdbe5facc0a48e22c8891a072ef8fb29fcc5acc
Pavel Palancica <pavel.palancica@gmail.com>
2025-05-04 16:37:05 +0300

    Added a Table View for displaying the products

--- FakeStore-iOS-App/ProductsViewController.swift
+++ FakeStore-iOS-App/ProductsViewController.swift
@@ -7,22 +7,59 @@
 
 import UIKit
 
-final class ProductsViewController: UIViewController {
-
+final class ProductsViewController: UIViewController, UITableViewDataSource {
+    
     private let api = FakeStoreAPI()
+    
+    private var products: [FakeStoreAPI.Product] = []
+    
+    private let productsTableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         title = "Products"
-        self.view.backgroundColor = .lightGray
+        productsTableView.dataSource = self
+        setupUI()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         fetchProducts()
     }
+    
+    func setupUI() {
+        view.addSubview(productsTableView)
+        productsTableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            productsTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            productsTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            productsTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            productsTableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
 }
+
+// MARK: - UITableViewDataSource Methods
+
+extension ProductsViewController {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let product = products[indexPath.row]
+        cell.textLabel?.text = product.title
+        return cell
+    }
+}
+
+// MARK: - Private Methods
 
 private extension ProductsViewController {
     
@@ -30,6 +67,10 @@ private extension ProductsViewController {
         api.getProducts { products, error in
             if let products {
                 print("Products: \(products)")
+                DispatchQueue.main.async {
+                    self.products = products
+                    self.productsTableView.reloadData()
+                }
             }
             if let error {
                 print("JSON decoding error: \(error)")
